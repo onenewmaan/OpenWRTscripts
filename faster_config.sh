1730645774b4f6215ea56107a31f2a17cd6e541a
generaluser0 <generaluser05@protonmail.ch>
2022-07-05 09:47:37 -0600

    add mqtt, install and enable openvpn

--- faster_config.sh
+++ faster_config.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#
 # Work in Progress, not tested! 
 # Use this script to auto-configure OpenWRT to a set of predifined settings
 # Use this script to quickly deploy and configure multiple modems on same of diferent networks.
@@ -7,28 +6,47 @@
 DIR=$(temp=$( realpath "$0" ) && dirname "$temp")
 echo $DIR
 
-#Update device name
+#__________ HOSTNAME ____________
+#Update device name:
+read -p 'Enter client name' CLIENTNAME
 read -p 'Enter device name / SSID name: ' HOSTNAME
 echo 'Setting up device name to: ' $HOSTNAME
-uci set system.system.hostname="$HOSTNAME"
-echo $(uci get system.system.hostname) > /proc/sys/kernel/hostname
-echo 'Device name updated to :' $(cat /proc/sys/kernel/hostname)
-
+if uci set system.system.hostname="$HOSTNAME" && echo $(uci get system.system.hostname) > /proc/sys/kernel/hostname; then
+        uci commit system
+        echo 'Device name updated to :' $(cat /proc/sys/kernel/hostname)
+else
+        echo 'Failed to update hostname, current hostname: ' $(cat /proc/sys/kernel/hostname)
+#___________ PASSWORD ___________
 #Update device passphrase
-read -p 'Enter device passphrase: ' NEWPASS
-echo 'Setting up the device passphrase'
+read -s 'Enter a new device passphrase: ' NEWPASS
 passwd << EOF
 $NEWPASS
 $NEWPASS
 EOF
 echo 'Done...'
 
+#__________ LAN ___________
 #Update device LAN address
 read -p 'Enter device LAN IP:' LANIPADDR
 echo 'Updating device LAN IP Address'
-uci set network.lan.ipaddr="$LANIPADDR"
+uci set network.lan.ipaddr="$LANIPADDR" && echo  "LAN IP Updated: " $(uci get network.lan.ipaddr) || echo "Failed"
+uci set network.lan.proto='static'
+uci set network.lan.netmask='255.255.255.0'
+uci commit network
+/etc/init.d/network restart
+
+#__________ DHCP ___________
+#Update dhcp settings
+uci set dhcp.lan=dhcp
+uci set dhcp.lan.interface='lan'
+uci set dhcp.lan.leasetime='12h'
+uci set dhcp.lan.start='130'
+uci set dhcp.lan.limit='120'
+uci commit dhcp
+/etc/init.d/odhcpd restart
 
 
+#__________ TIMEZONE ZONENAME ___________
 #Check and update timezone
 echo 'Device timezone is set to : ' $(uci get system.system.timezone)
 if [ $TIMEZONE != 'MDT']; then
@@ -55,6 +73,7 @@ if [ $TIMEZONE != 'MDT']; then
 else
         echo 'Zonename unchanged'
 
+#____________ WIRELESS _____________
 #Setup wireless access point
 uci set wireless.radio0=wifi-device
 uci set wireless.radio0.type='mac80211'
@@ -74,56 +93,77 @@ uci set wireless.@wifi-iface[0].wifi_id='wifi1'
 uci set wireless.@wifi-iface[0].key='$NEWPASS'
 uci set wireless.@wifi-iface[0].ssid='$HOSTNAME'
 
-#Install packages
+
+#_____________MQTT___________
+#Install MQTT packages.
+opkg install mosquitto-ssl
+opkg install mqtt-modbus-gateway
+opkg install mqtt_pub
+opkg install mtd
+#Create a new password file and add user riot.
+mosquitto_passwd -c /etc/mosquitto/passwd riot <<EOF
+riot
+EOF
+
+#Overwrite mosquitto.conf file.
+echo "user root
+port 1883
+allow_anonymous false
+password_file /etc/mosquitto/passwd" > /etc/mosquitto/mosquitto.conf
+#Enable mosquitto service
+service mosquitto start
+# NOTE!!! Above script works if the device has not been epanded with an SD card,
+# if so, update the mosquitto configuration with the following code
+#uci set mosquitto.mqtt.password_file='/etc/mosquitto/passwd'
+#uci commit
+#/etc/init.d/mosquitto restart
+
+#___________PYTHON____________
+#Install python packages
 opkg update
 opkg install python3-light
 opkg install python3-pip
 okpg install python3-pyserial
 okpg install python3-paho-mqtt
-okpg install mosquitto-ssl
-opkg install openvpn-openssl
-
 #Install wheel
 pip3 install wheel
 
-#Clone auto exect files
-mkdir -p ~/auto_src/root/src
-cd ~/auto_src/root/src
+#__________ GIT __________
+#Install git and clone riot exect repos
+opkg install git git-http
 
-echo "Extracting auto exect files from TRAC Git Server"
+#___________RIOT__________
+#Clone riot repositories
+mkdir -p ~/riot_src/root/src
+cd ~/riot_src/root/src
+echo "Extracting riot exect files from TRAC Git Server"
 read -p "Enter your TRAC username: " TRAC_USER
 read -s "Enter your TRAC userpass: " TRAC_USER_PASS
-cat << EOF 
-git clone ssh://$TRAC_USER@domain.com:/srv/git/auto/autoexec/auto_exec
+cat << EOF
+git clone ssh://$TRAC_USER@domain.com:/srv/git/exec/exec
 $TRAC_USER_PASS
-git clone ssh://$TRAC_USER@domain.com:/srv/git/auto/autoexec/channels/auto_exec_channel_sockets
-$TRAC_USER_PASS
-git clone ssh://$TRAC_USER@domain.com:/srv/git/auto/autoexec/channels/auto_exec_channel_tty
-$TRAC_USER_PASS
-git clone ssh://$TRAC_USER@domain.com:/srv/git/auto/autoexec/drivers/auto_exec_driver_modbus
-$TRAC_USER_PASS
-git clone ssh://$TRAC_USER@domain.com:/srv/git/auto/autoexec/drivers/auto_exec_driver_mercury
-$TRAC_USER_PASS
-git clone ssh://$TRAC_USER@domain.com:/srv/git/auto/autoexec/routines/auto_exec_routine_icsgw
-$TRAC_USER_PASS
-git clone ssh://$TRAC_USER@domain.com:/srv/git/auto/autoexec/routines/auto_exec_routine_hornermb
-$TRAC_USER_PASS
+git clone ssh://$TRAC_USER@domain.com:/srv/git/exec/channels/exec_channel_sockets
+git clone ssh://$TRAC_USER@domain.com:/srv/git/exec/channels/exec_channel_tty
+git clone ssh://$TRAC_USER@domain.com:/srv/git/exec/drivers/exec_driver_modbus
+git clone ssh://$TRAC_USER@domain.com:/srv/git/exec/drivers/exec_driver_mercury
+git clone ssh://$TRAC_USER@domain.com:/srv/git/exec/routines/exec_routine_icsgw
+git clone ssh://$TRAC_USER@domain.com:/srv/git/exec/routines/exec_routine_hornermb
 EOF
 
-#Configure MQTT broker
-echo "allow_anonymous false
-password_file /etc/mosquitto/passwd" > /etc/mosquitto/mosquitto.conf
+#Install exec
+pip3 install ~/src/exec/exec
+pip3 install ~/src/exec/channels/exec_channel_sockets
+pip3 install ~/src/exec/channels/exec_channel_tty
+pip3 install ~/src/exec/drivers/exec_driver_modbus
+pip3 install ~/src/exec/drivers/exec_driver_mercury
+pip3 install ~/src/exec/routines/exec_routine_icsgw
+pip3 install ~/src/exec/routines/exec_routine_hornermb
 
-#Install autoexec
-pip3 install ~/src/autoexec/auto_exec
-pip3 install ~/src/autoexec/channels/auto_exec_channel_sockets
-pip3 install ~/src/autoexec/channels/auto_exec_channel_tty
-pip3 install ~/src/autoexec/drivers/auto_exec_driver_modbus
-pip3 install ~/src/autoexec/drivers/auto_exec_driver_mercury
-pip3 install ~/src/autoexec/routines/auto_exec_routine_icsgw
-pip3 install ~/src/autoexec/routines/auto_exec_routine_hornermb
+#___________ OPENVPN ___________
+#Instal and configure an openvpn-client
+opkg install openvpn-easy-rsa openvpn-openssl
 
-#commit changes and restart netwrok
-uci commit system
-uci commit netwrok
-/etc/init.d/network restart
+#Autostart on router startup
+/etc/init.d/openvpn enable
+
+#Configure instance as client
